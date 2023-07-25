3a7d2035e2250cd55ad94a15fa2a8118f5f4516a
hebozhe <hebozhe@gmail.com>
2023-07-24 21:59:45 -0700

    modified:   Deriver/Deriver.py 	modified:   Deriver/bzns/primitives.py

--- Deriver/Deriver.py
+++ Deriver/Deriver.py
@@ -1,5 +1,5 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
-from bzns.primitives import SYM_CONV
+from Deriver.bzns.primitives import SYM_CONV
 import reflex as rx
 from rxconfig import config
 
@@ -11,36 +11,58 @@ class State(rx.State):
     """The app state."""
 
 
+class InputState(rx.State):
+    """
+    The state of the input.
+    """
+
+    fmla_list: list[str] = ["A", "A→B", "B"]
+    label_text: str = f"Derive: A, A→B ⊢ B"
+
+    def update_fmla_list_and_label(self, text: str) -> None:
+        """
+        Format the label text to resemble a Gentzen sequent,
+        and update the formula list to reflect the inputs.
+
+        Args:
+            text (str): The input string to be checked.
+
+        Returns
+            None
+        """
+
+        # Update the label text.
+        for sym_str, conv in SYM_CONV.items():
+            text = text.replace(sym_str, conv)
+        self.fmla_list = [f.strip() for f in text.split(",") if f.strip()]
+
+        if not self.fmla_list:
+            self.label_text = "Derive something."
+            return None
+
+        prems: str = ", ".join(self.fmla_list[:-1]) if self.fmla_list[:-1] else ""
+        self.label_text = f"Derive: {prems} ⊢ {self.fmla_list[-1]}."
+
+        return None
+
+
 def index() -> rx.Component:
     """
     This function houses the frontend components for the homepage.
     """
-    return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
+    return rx.vstack(
+        *(
+            rx.text(InputState.label_text),
+            rx.input(
+                placeholder="A, A→B, B",
+                on_change=InputState.update_fmla_list_and_label,
             ),
-            spacing="1.5em",
-            font_size="2em",
-            padding_top="10%",
-        ),
+            rx.text("Separate each formula with commas."),
+        )
     )
 
 
 # Add state and page to the app.
-app = rx.App(state=State)
+app = rx.App(state=InputState)
 app.add_page(index)
 app.compile()
