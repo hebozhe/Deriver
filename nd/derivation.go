9ca367d7ebc0bbf3732fea7a024fb1bb38e5281c
Joshua Harwood (何博哲) <hebozhe@gmail.com>
2025-12-30 00:57:36 -0700

    Built out an initial strategy for derivations based on an incremental strengthening of the inferential permissibility, which correspond to well-known restricted logical axiomatizations, from implicational to classical logic, from modal K to S5 (here, KD4B).

---
+++ nd/derivation.go
@@ -0,0 +1,122 @@
+package nd
+
+import (
+	"Deriver/fmla"
+	"Deriver/nd/pr"
+)
+
+func pumpIntroductions(prf *pr.Proof, iFuncs []ndRuleFunc) (added uint, met bool) {
+	var (
+		iFunc  ndRuleFunc
+		prfsI  []*pr.Proof
+		prfI   *pr.Proof
+		addedI uint
+	)
+
+	if _, _, met = prf.HeadGoalMet(); !met {
+		for _, iFunc = range iFuncs {
+			added += iFunc(prf)
+		}
+
+		prfsI = collectInnerProofs(prf)
+
+		for _, prfI = range prfsI {
+			addedI, _ = pumpIntroductions(prfI, iFuncs)
+
+			added += addedI
+		}
+	}
+
+	_, _, met = prf.HeadGoalMet()
+
+	return
+}
+
+func pumpEliminations(prf *pr.Proof, eFuncs []ndRuleFunc) (added uint, met bool) {
+	var (
+		eFunc  ndRuleFunc
+		prfsI  []*pr.Proof
+		prfI   *pr.Proof
+		addedI uint
+	)
+
+	if _, _, met = prf.HeadGoalMet(); !met {
+		for _, eFunc = range eFuncs {
+			added += eFunc(prf)
+		}
+
+		prfsI = collectInnerProofs(prf)
+
+		for _, prfI = range prfsI {
+			addedI, _ = pumpEliminations(prfI, eFuncs)
+
+			added += addedI
+		}
+	}
+
+	_, _, met = prf.HeadGoalMet()
+
+	return
+}
+
+func Derive(goal *fmla.WffTree, prems ...*fmla.WffTree) (prf *pr.Proof) {
+	var (
+		infS           InferStrength
+		modS           ModalStrength
+		iFuncs, eFuncs []ndRuleFunc
+		added, popped  uint
+		met            bool
+	)
+
+	infS, modS = Implicational, SystemK
+
+	prf = pr.NewBaseProof(goal, prems...)
+
+	iFuncs, eFuncs = ruleFuncsByStrengths(infS, modS)
+
+	for {
+		// 1. Apply introduction rules until no unique lines are produced or the head goal is met.
+		// 2. If (1) met the head goal, exit! If new, unique lines are produced,
+		//    pop subgoals and return to (1). Otherwise, move to (3).
+		if added, met = pumpIntroductions(prf, iFuncs); met {
+			break
+		} else if 0 < added {
+			popped += prf.PopMetSubgoals()
+
+			continue
+		}
+
+		// 3. Continue applying elimination rules until no unique lines are produced or the head goal is met.
+		// 4. If (3) met the head goal, exit! If new, unique lines are produced,
+		//    pop subgoals and return to (1). Otherwise, move to (5).
+		if added, met = pumpEliminations(prf, eFuncs); met {
+			break
+		} else if 0 < added {
+			popped += prf.PopMetSubgoals()
+
+			continue
+		}
+
+		// 5. Create inner proofs to facilitate both introduction and elimination rules.
+		// 6. If (5) produced new, unique inner proofs, return to (1).
+		//    Otherwise, move to (7).
+		if added = SetInnerIntroProofs(prf) + SetInnerElimProofs(prf); 0 < added {
+			continue
+		}
+
+		// 7. Increment the modal strength; or, if it's already at KD4B, increment the inferential strength,
+		//    reset the modal strength to K. If the modal strength is already at Classical KD4B, exit in failure!
+		//    Otherwise, return to (1).
+		if infS == Classical && modS == SystemKD4B {
+			break
+		}
+
+		if modS == incModalStrength(modS) {
+			infS, modS = incInferStrength(infS), SystemK
+		} else {
+			modS = incModalStrength(modS)
+		}
+	}
+
+	return
+}
