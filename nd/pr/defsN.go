1a567c5127675cc7067d4803ae2cfdbef0039a2a
Joshua Harwood (何博哲) <hebozhe@gmail.com>
2026-03-11 12:29:23 -0700

    Extended modalities to include axiomatizations that exclude □IK (Necessitation).

--- nd/pr/defsN.go
+++ nd/pr/defsN.go
@@ -1,8 +1,6 @@
 package pr
 
-import (
-	"slices"
-)
+import "strings"
 
 type ndRuleFunc func(prf *Proof) (added uint)
 
@@ -21,17 +19,98 @@ const (
 const (
 	NoSystem ModStrength = iota
 	SystemK
+	SystemD
+	SystemM
+	System4
+	SystemB
+	// Extensions from K:
 	SystemKD
+	SystemKM
 	SystemK4
 	SystemKB
-	SystemKM // Proves all that KDM does.
+	// Extensions from D:
+	SystemDM
+	SystemD4
+	SystemDB
+	// Extensions from M:
+	SystemM4
+	SystemMB
+	// Extensions from KD:
+	SystemKDM
 	SystemKD4
 	SystemKDB
+	// Extensions from KM:
+	SystemKM4
+	SystemKMB
+	// Extensions from K4:
 	SystemK4B
-	SystemKM4  // Proves all that KDM4 does.
-	SystemKMB  // Proves all that KDMB does.
-	SystemKM4B // Proves all the KD4B and KDM4B does.
+	// Extensions from DM:
+	SystemDM4
+	SystemDMB
+	// Extensions from D4:
+	SystemD4B
+	// Extensions from M4:
+	SystemM4B
+	// Extensions from KDM:
+	SystemKDM4
+	SystemKDMB
+	// Extensions from KD4:
+	SystemKD4B
+	// Extensions from KM4:
+	SystemKM4B
+	// Extensions from DM4:
+	SystemDM4B
+	// Extensions from KDM4:
+	SystemKDM4B
 )
+
+var modalSystemToString map[ModStrength]string = map[ModStrength]string{
+	NoSystem: "NoSystem",
+	SystemK:  "K",
+	SystemD:  "D",
+	SystemM:  "M",
+	System4:  "4",
+	SystemB:  "B",
+	// Extensions from K:
+	SystemKD: "KD",
+	SystemKM: "KM",
+	SystemK4: "K4",
+	SystemKB: "KB",
+	// Extensions from D:
+	SystemDM: "DM",
+	SystemD4: "D4",
+	SystemDB: "DB",
+	// Extensions from M:
+	SystemM4: "M4",
+	SystemMB: "MB",
+	// Extensions from KD:
+	SystemKDM: "KDM",
+	SystemKD4: "KD4",
+	SystemKDB: "KDB",
+	// Extensions from KM:
+	SystemKM4: "KM4",
+	SystemKMB: "KMB",
+	// Extensions from K4:
+	SystemK4B: "K4B",
+	// Extensions from DM:
+	SystemDM4: "DM4",
+	SystemDMB: "DMB",
+	// Extensions from D4:
+	SystemD4B: "D4B",
+	// Extensions from M4:
+	SystemM4B: "M4B",
+	// Extensions from KDM:
+	SystemKDM4: "KDM4",
+	SystemKDMB: "KDMB",
+	// Extensions from KD4:
+	SystemKD4B: "KD4B",
+	// Extensions from KM4:
+	SystemKM4B: "KM4B",
+	// Extensions from DM4:
+	SystemDM4B: "DM4B",
+	// Extensions from KDM4:
+	SystemKDM4B: "KDM4B",
+}
 
 func inferStrengthOfNDRule(rule NDRule) (infS InfStrength) {
 	switch rule {
@@ -81,158 +160,43 @@ func InferStrengthOfNDRules(rules ...NDRule) (infS InfStrength) {
 	return
 }
 
-func modalStrengthOfNDRule(rule NDRule) (modS ModStrength) {
+func minimalModalStrengthOfNDRule(rule NDRule) (modM ModStrength) {
 	var infS InfStrength
 
 	switch rule {
 	case BoxIntro, BoxElim, DiamondElim, DiamondIntro:
-		modS = NoSystem
+		modM = NoSystem
 	case IntroK:
-		modS = SystemK
+		modM = SystemK
 	case ElimD:
-		modS = SystemKD
+		modM = SystemD
 	case IntroM, ElimM:
-		modS = SystemKM
+		modM = SystemM
 	case Intro4, Elim4:
-		modS = SystemK4
+		modM = System4
 	case IntroB, ElimB:
-		modS = SystemKB
+		modM = SystemB
 	default:
 		if infS = inferStrengthOfNDRule(rule); !(infS < NoInference) {
-			modS = NoSystem
+			modM = NoSystem
 		}
 	}
-
-	return
-}
-
-func ModalStrengthOfNDRules(rules ...NDRule) (modS ModStrength) {
-	var (
-		ruleToModS map[string]ModStrength
-		rule       NDRule
-		key        []string
-		s          string
-	)
-
-	ruleToModS = map[string]ModStrength{
-		"":      NoSystem,
-		"K":     SystemK,
-		"KD":    SystemKD,
-		"K4":    SystemK4,
-		"KB":    SystemKB,
-		"KDM":   SystemKM,
-		"KD4":   SystemKD4,
-		"KDB":   SystemKDB,
-		"K4B":   SystemK4B,
-		"KDM4":  SystemKM4,
-		"KDMB":  SystemKMB,
-		"KDM4B": SystemKM4B,
-	}
-
-	for _, rule = range rules {
-		if modS = modalStrengthOfNDRule(rule); modS == NoSystem {
-			continue
-		}
-
-		switch modS {
-		case SystemK:
-			key = append(key, "K")
-		case SystemKD:
-			key = append(key, "K", "D")
-		case SystemK4:
-			key = append(key, "K", "4")
-		case SystemKB:
-			key = append(key, "K", "B")
-		case SystemKM:
-			key = append(key, "K", "D", "M")
-		case SystemKD4:
-			key = append(key, "K", "D", "4")
-		case SystemKDB:
-			key = append(key, "K", "D", "B")
-		case SystemK4B:
-			key = append(key, "K", "4", "B")
-		case SystemKM4:
-			key = append(key, "K", "D", "M", "4")
-		case SystemKMB:
-			key = append(key, "K", "D", "M", "B")
-		case SystemKM4B:
-			key = append(key, "K", "D", "M", "4", "B")
-		default:
-			panic("Failed to capture NDRule.")
-		}
-	}
-
-	s = ""
-
-	if slices.Contains(key, "K") {
-		s += "K"
-	}
-
-	if slices.Contains(key, "D") {
-		s += "D"
-	}
-
-	if slices.Contains(key, "M") {
-		s += "M"
-	}
-
-	if slices.Contains(key, "4") {
-		s += "4"
-	}
-
-	if slices.Contains(key, "B") {
-		s += "B"
-	}
-
-	modS = ruleToModS[s]
 
 	return
 }
 
 func IsAllowedModality(rule NDRule, modS ModStrength) (is bool) {
 	var (
-		modR        ModStrength
-		modSToAllow map[ModStrength][]NDRule
+		modM   ModStrength
+		sM, sS string
 	)
 
-	if modR = modalStrengthOfNDRule(rule); modR == NoSystem {
+	if modM = minimalModalStrengthOfNDRule(rule); modM == NoSystem {
 		is = true
 	} else {
-		modSToAllow = map[ModStrength][]NDRule{}
+		sM, sS = modalSystemToString[modM], modalSystemToString[modS]
 
-		modSToAllow[SystemK] = []NDRule{IntroK}
-
-		modSToAllow[SystemKD] = append([]NDRule{}, modSToAllow[SystemK]...)
-		modSToAllow[SystemKD] = append(modSToAllow[SystemKD], ElimD)
-
-		modSToAllow[SystemK4] = append([]NDRule{}, modSToAllow[SystemK]...)
-		modSToAllow[SystemK4] = append(modSToAllow[SystemK4], Intro4, Elim4)
-
-		modSToAllow[SystemKB] = append([]NDRule{}, modSToAllow[SystemK]...)
-		modSToAllow[SystemKB] = append(modSToAllow[SystemKB], IntroB, ElimB)
-
-		modSToAllow[SystemKM] = append([]NDRule{}, modSToAllow[SystemKD]...)
-		modSToAllow[SystemKM] = append(modSToAllow[SystemKM], IntroM, ElimM)
-
-		modSToAllow[SystemKD4] = append([]NDRule{}, modSToAllow[SystemKD]...)
-		modSToAllow[SystemKD4] = append(modSToAllow[SystemKD4], Intro4, Elim4)
-
-		modSToAllow[SystemKDB] = append([]NDRule{}, modSToAllow[SystemKD]...)
-		modSToAllow[SystemKDB] = append(modSToAllow[SystemKDB], IntroB, ElimB)
-
-		modSToAllow[SystemK4B] = append([]NDRule{}, modSToAllow[SystemK4]...)
-		modSToAllow[SystemK4B] = append(modSToAllow[SystemK4B], IntroB, ElimB)
-
-		modSToAllow[SystemKM4] = append([]NDRule{}, modSToAllow[SystemKM]...)
-		modSToAllow[SystemKM4] = append(modSToAllow[SystemKM4], Intro4, Elim4)
-
-		modSToAllow[SystemKMB] = append([]NDRule{}, modSToAllow[SystemKM]...)
-		modSToAllow[SystemKMB] = append(modSToAllow[SystemKMB], IntroB, ElimB)
-
-		modSToAllow[SystemKM4B] = append([]NDRule{}, modSToAllow[SystemKM4]...)
-		modSToAllow[SystemKM4B] = append(modSToAllow[SystemKM4B], IntroB, ElimB)
-
-		is = slices.Contains(modSToAllow[modS], rule)
+		is = strings.Contains(sS, sM)
 	}
 
 	return
