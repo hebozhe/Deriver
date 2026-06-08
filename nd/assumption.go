71a92c659b8b21f042f9717c04032322103dedbd
Joshua Harwood (何博哲) <hebozhe@gmail.com>
2026-06-07 18:42:53 -0700

    Adjusted `pushAssumptions` to focus on the weakest required logics, and also added some introduction checks for the created assumption lines.

--- nd/assumption.go
+++ nd/assumption.go
@@ -3,6 +3,7 @@ package nd
 import (
 	"Deriver/fmla"
 	"Deriver/nd/pr"
+	"fmt"
 )
 
 func makeNewInnerProof(wffA, wffG *fmla.Wff, prf *pr.Proof, purp pr.NDRule, js ...*pr.Line) (prfI *pr.Proof, ok bool) {
@@ -63,14 +64,116 @@ func makeNewInnerProof(wffA, wffG *fmla.Wff, prf *pr.Proof, purp pr.NDRule, js .
 	return
 }
 
+func (drv *Deriver) getElimGoalsFromNewPrf(prf *pr.Proof) (wffsE []*fmla.Wff) {
+	var (
+		ln0                   *pr.Line
+		wff, wffA, wffB, wffG *fmla.Wff
+		mop                   fmla.Symbol
+		prfI                  *pr.Proof
+		ok                    bool
+	)
+
+	ln0 = prf.GetLineAtIndex(0)
+
+	wff, wffG = ln0.GetWff(), prf.GetWffG()
+
+	mop = fmla.GetWffMop(wff)
+
+	switch mop {
+	case fmla.Neg:
+		if pr.Positive < drv.InfS { // At least MPL...
+			wffA, _ = fmla.GetWffSubformulae(wff)
+
+			wffsE = append(wffsE, wffA)
+		}
+	case fmla.Vee:
+		if pr.Implicational < drv.InfS { // At least PPL...
+			wffA, wffB = fmla.GetWffSubformulae(wff)
+
+			wffA, wffB = fmla.NewBinaryWff(fmla.To, wffA, wffG), fmla.NewBinaryWff(fmla.To, wffB, wffG)
+
+			wffsE = append(wffsE, wffA, wffB)
+		}
+	case fmla.Wedge:
+		if pr.Implicational < drv.InfS { // At least PPL...
+			wffA, wffB = fmla.GetWffSubformulae(wff)
+
+			wffsE = append(wffsE, wffA, wffB)
+		}
+	case fmla.To:
+		if pr.NoInference < drv.InfS { // At least TPL...
+			wffA, _ = fmla.GetWffSubformulae(wff)
+
+			wffsE = append(wffsE, wffA)
+		}
+	case fmla.Iff:
+		if pr.Implicational < drv.InfS { // At least PPL...
+			wffA, wffB = fmla.GetWffSubformulae(wff)
+
+			wffsE = append(wffsE, wffA, wffB)
+		}
+	case fmla.ForAll: // ∀E is a one-premise rule. Skip!
+	case fmla.Exists:
+		if pr.Implicational < drv.InfS { // At least P[12]QL...
+			if prfI, ok = makeNewInnerProof(wff, wffG, prf, pr.ExistsElim, ln0); ok {
+				_ = prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffG, prfI)
+			}
+		}
+	case fmla.Box: // What to do here?
+	case fmla.Diamond:
+		var (
+			prfsO []*pr.Proof
+			prfO  *pr.Proof
+			bot   *fmla.Wff
+		)
+
+		prfsO = prf.GetOuterProofsAtModalDistance(true, 0)
+
+		if pr.Implicational < drv.InfS && pr.HasModality(drv.ModS, pr.ModalK) { // At least PPL+K...
+			wffA = fmla.RetrieveSubformula(wff, "L!")
+
+			for _, prfO = range prfsO {
+				if wffB = prfO.GetWffG(); fmla.GetWffMop(wffB) != fmla.Diamond {
+					continue
+				}
+
+				wffB = fmla.RetrieveSubformula(wffB, "L!")
+
+				if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.DiamondElim, ln0); ok {
+					_ = prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffB, prfI)
+				}
+			}
+		}
+
+		if pr.Positive < drv.InfS && pr.HasModality(drv.ModS, pr.ModalK) { // At least MPL+K...
+			wffA, bot = fmla.RetrieveSubformula(wff, "L!"), fmla.NewAtomicWff(fmla.Bot)
+
+			for _, prfO = range prfsO {
+				if wffB = prfO.GetWffG(); !fmla.IsIdentical(wffB, bot) {
+					continue
+				}
+
+				if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.DiamondElim, ln0); ok {
+					_ = prf.InsertInnerProofs(prfI)
+
+					break
+				}
+			}
+		}
+	default:
+	}
+
+	return
+}
+
 func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 	var (
-		wffA, wffB *fmla.Wff
-		prfI       *pr.Proof
-		mop        fmla.Symbol
-		ok         bool
-		pc         fmla.Predicate
-		ln0        *pr.Line
+		wffA, wffB, wffE *fmla.Wff
+		prfI             *pr.Proof
+		mop              fmla.Symbol
+		ok               bool
+		pc               fmla.Predicate
+		ln0              *pr.Line
 	)
 
 	mop = fmla.GetWffMop(wffG)
@@ -105,35 +208,13 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 				if fmla.GetWffMop(wffA) == fmla.Neg {
 					wffA = fmla.RetrieveSubformula(wffA, "L!")
 
-					tot += drv.pushAssumptions(wffA, prf)
+					tot += drv.pushAssumptions(wffA, prfI)
 				}
 			}
 		}
 	case fmla.Wedge: // ⊢ A∧B
 		switch drv.InfS {
-		case pr.Classical:
-			// ¬¬A, ¬¬B ⊢ A, B ⊢ A∧B
-			wffA, wffB = fmla.GetWffSubformulae(wffG)
-			wffA, wffB = fmla.FillTemplate("¬¬(?)", wffA), fmla.FillTemplate("¬¬(?)", wffB)
-
-			tot += drv.pushAssumptions(wffA, prf) + drv.pushAssumptions(wffB, prf)
-
-			fallthrough
-		case pr.Intuitionistic:
-			// [A ... ⊥], [¬A ... ⊥] ⊢ ¬A, ¬¬A ⊢ ⊥ ⊢ A∧B
-			// [B ... ⊥], [¬B ... ⊥] ⊢ ¬B, ¬¬B ⊢ ⊥ ⊢ A∧B
-			wffA, wffB = fmla.GetWffSubformulae(wffG)
-
-			wffA, wffB = fmla.FillTemplate("¬(?)", wffA), fmla.FillTemplate("¬(?)", wffB)
-
-			tot += drv.pushAssumptions(wffA, prf) + drv.pushAssumptions(wffB, prf)
-
-			wffA, wffB = fmla.FillTemplate("¬(?)", wffA), fmla.FillTemplate("¬(?)", wffB)
-
-			tot += drv.pushAssumptions(wffA, prf) + drv.pushAssumptions(wffB, prf)
-
-			fallthrough
-		case pr.Minimal, pr.Positive:
+		case pr.Classical, pr.Intuitionistic, pr.Minimal, pr.Positive:
 			// A, B ⊢ A∧B
 			wffA, wffB = fmla.GetWffSubformulae(wffG)
 
@@ -157,14 +238,11 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 				ln0 = prfI.GetLineAtIndex(0)
 
 				_ = ln0.SetExtended(true)
+
+				for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+					tot += drv.pushAssumptions(wffE, prfI)
+				}
 			}
-
-			// ¬¬A ⊢ A ⊢ A∨B
-			// ¬¬B ⊢ B ⊢ A∨B
-			wffA, wffB = fmla.GetWffSubformulae(wffG)
-			wffA, wffB = fmla.FillTemplate("¬¬(?)", wffA), fmla.FillTemplate("¬¬(?)", wffB)
-
-			tot += drv.pushAssumptions(wffA, prf) + drv.pushAssumptions(wffB, prf)
 
 			fallthrough
 		case pr.Intuitionistic, pr.Minimal, pr.Positive:
@@ -176,50 +254,16 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 		}
 	case fmla.To: // ⊢ A→B
 		switch drv.InfS {
-		case pr.Classical:
-			// [¬(A→B), ¬¬A, ¬B, ... ⊥] ⊢ ¬¬(A→B) ⊢ A→B
-			// wffA, wffB = fmla.NewUnaryWff(fmla.Neg, wffG), fmla.NewAtomicWff(fmla.Bot)
-
-			// if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.NegIntro); ok {
-			// 	tot += prf.InsertInnerProofs(prfI)
-
-			// 	// Accelerate the proof with [¬(A→B), ... ] ⊢ [¬(A→B), ¬¬A, ¬B... ].
-			// 	wffA, wffB = fmla.GetWffSubformulae(wffG)
-			// 	wffA, wffB = fmla.FillTemplate("¬¬(?)", wffA), fmla.NewUnaryWff(fmla.Neg, wffB)
-
-			// 	tot += drv.pushAssumptions(wffA, prfI) + drv.pushAssumptions(wffB, prfI)
-
-			// 	ln0 = prfI.GetLineAtIndex(0)
-
-			// 	_ = ln0.SetExtended(true)
-			// }
-
-			fallthrough
-		case pr.Intuitionistic:
-			// [A ... ⊥] ⊢ ¬A ⊢ A→B
-			wffA, _ = fmla.GetWffSubformulae(wffG)
-			wffA = fmla.NewUnaryWff(fmla.Neg, wffA)
-
-			tot += drv.pushAssumptions(wffA, prf)
-
-			fallthrough
-		case pr.Minimal:
-			// If ¬B, then [A ... ⊥] ⊢ ¬A ⊢ A→¬B
-			wffA, wffB = fmla.GetWffSubformulae(wffG)
-
-			if fmla.GetWffMop(wffB) == fmla.Neg {
-				wffA = fmla.NewUnaryWff(fmla.Neg, wffA)
-
-				tot += drv.pushAssumptions(wffA, prf)
-			}
-
-			fallthrough
-		case pr.Positive, pr.Implicational:
+		case pr.Classical, pr.Intuitionistic, pr.Minimal, pr.Positive, pr.Implicational:
 			// [A ... B] ⊢ A→B
 			wffA, wffB = fmla.GetWffSubformulae(wffG)
 
 			if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.ToIntro); ok {
 				tot += prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffB, prfI)
+
+				for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+					tot += drv.pushAssumptions(wffE, prfI)
+				}
 			}
 		}
 	case fmla.Iff: // ⊢ A↔B
@@ -230,18 +274,21 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 
 			if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.ToIntro); ok {
 				tot += prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffB, prfI)
+
+				for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+					tot += drv.pushAssumptions(wffE, prfI)
+				}
 			}
 
 			if prfI, ok = makeNewInnerProof(wffB, wffA, prf, pr.ToIntro); ok {
 				tot += prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffA, prfI)
+
+				for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+					tot += drv.pushAssumptions(wffE, prfI)
+				}
 			}
 		}
 	case fmla.ForAll: // ⊢ ∀[x|X]A
-		// var (
-		// 	pv fmla.Predicate
-		// 	av fmla.Argument
-		// )
-
 		switch drv.InfS {
 		case pr.Classical, pr.Intuitionistic, pr.Minimal, pr.Positive:
 			// [⊤ ... A(t/x|T/X)] ⊢ ∀[x|X]A
@@ -251,6 +298,10 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 				wffB = prfI.GetWffG() // makeNewInnerProof freshly instantiates for you.
 
 				tot += prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffB, prfI)
+
+				for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+					tot += drv.pushAssumptions(wffE, prfI)
+				}
 			}
 		}
 	case fmla.Exists: // ⊢ ∃[x|X]A
@@ -264,10 +315,28 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 
 		switch drv.InfS {
 		case pr.Classical:
-			// ¬¬∃[x|X]A ⊢ ∃[x|X]A
-			wffA = fmla.FillTemplateWithLocales("¬¬(?)", wffG, "!")
+			// [¬∃[x|X]A ... ⊥] ⊢ ¬¬∃[x|X]A ⊢ ∃[x|X]A
+			var (
+				pv  fmla.Predicate
+				av  fmla.Argument
+				tmp string
+			)
 
-			tot += drv.pushAssumptions(wffA, prf)
+			wffA, wffB = fmla.NewUnaryWff(fmla.Neg, wffG), fmla.NewAtomicWff(fmla.Bot)
+
+			if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.NegIntro); ok {
+				tot += prf.InsertInnerProofs(prfI)
+
+				if pv, av = fmla.GetWffVars(wffG); pv != 0 {
+					tmp = fmt.Sprintf("∀%c¬(?)", pv)
+				} else if av != 0 {
+					tmp = fmt.Sprintf("∀%c¬(?)", av)
+				}
+
+				wffA = fmla.FillTemplateWithLocales(tmp, wffG, "L!")
+
+				tot += drv.pushAssumptions(wffA, prfI)
+			}
 
 			fallthrough
 		case pr.Intuitionistic, pr.Minimal, pr.Positive:
@@ -310,27 +379,29 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 	case fmla.Box: // ⊢ □A
 		switch drv.InfS {
 		case pr.Classical:
-			if pr.IsAllowedModality(pr.DiamondElim, drv.ModS) {
-				wffA = fmla.FillTemplateWithLocales("¬¬□(?)", wffG, "L!")
+			wffA = fmla.FillTemplateWithLocales("¬¬□(?)", wffG, "L!")
 
-				tot += drv.pushAssumptions(wffA, prf)
-			}
+			tot += drv.pushAssumptions(wffA, prf)
 
 			fallthrough
 		case pr.Intuitionistic, pr.Minimal, pr.Positive:
-			if pr.IsAllowedModality(pr.BoxIntro, drv.ModS) {
+			if pr.HasModality(drv.ModS, pr.ModalK) {
 				// [⊤ ... A] ⊢ □A
 				wffA, wffB = fmla.NewAtomicWff(fmla.Top), fmla.RetrieveSubformula(wffG, "L!")
 
 				if prfI, ok = makeNewInnerProof(wffA, wffB, prf, pr.BoxIntro); ok {
 					tot += prf.InsertInnerProofs(prfI) + drv.pushAssumptions(wffB, prfI)
+
+					for _, wffE = range drv.getElimGoalsFromNewPrf(prfI) {
+						tot += drv.pushAssumptions(wffE, prfI)
+					}
 				}
 			}
 		}
 	case fmla.Diamond: // ⊢ ◇A
 		switch drv.InfS {
 		case pr.Classical:
-			if pr.IsAllowedModality(pr.DiamondIntro, drv.ModS) {
+			if pr.HasModality(drv.ModS, pr.ModalK) {
 				// ¬□¬A ⊢ ◇A
 
 				wffA = fmla.FillTemplateWithLocales("¬□¬(?)", wffG, "L!")
@@ -338,16 +409,30 @@ func (drv *Deriver) pushAssumptions(wffG *fmla.Wff, prf *pr.Proof) (tot int) {
 				tot += drv.pushAssumptions(wffA, prf)
 			}
 
+			if pr.HasModality(drv.ModS, pr.ModalD) {
+				// ¬¬□A ⊢ □A ⊢ ◇A
+				wffA = fmla.FillTemplateWithLocales("¬¬□(?)", wffG, "L!")
+
+				tot += drv.pushAssumptions(wffA, prf)
+			}
+
+			if pr.HasModality(drv.ModS, pr.ModalM) {
+				// ¬¬A ⊢ A ⊢ ◇A
+				wffA = fmla.FillTemplateWithLocales("¬¬(?)", wffG, "L!")
+
+				tot += drv.pushAssumptions(wffA, prf)
+			}
+
 			fallthrough
 		case pr.Intuitionistic, pr.Minimal, pr.Positive:
-			if pr.IsAllowedModality(pr.ElimD, drv.ModS) {
+			if pr.HasModality(drv.ModS, pr.ModalD) {
 				// □A ⊢ ◇A
 				wffA = fmla.FillTemplateWithLocales("□(?)", wffG, "L!")
 
 				tot += drv.pushAssumptions(wffA, prf)
 			}
 
-			if pr.IsAllowedModality(pr.IntroM, drv.ModS) {
+			if pr.HasModality(drv.ModS, pr.ModalM) {
 				// A ⊢ ◇A
 				wffA = fmla.RetrieveSubformula(wffG, "L!")
 
